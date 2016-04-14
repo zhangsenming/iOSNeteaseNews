c56369af0f9712c6343dd6fae5e5458311d6a3af
Sonmin <1144251749@qq.com>
2016-04-14 17:17:36 +0800

    初始化项目

---
+++ NeteaseNews/NeteaseNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NeteaseNews
+//
+//  Created by 张森明 on 16/4/14.
+//  Copyright © 2016年 sonmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NeteaseNews/NeteaseNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NeteaseNews
+//
+//  Created by 张森明 on 16/4/14.
+//  Copyright © 2016年 sonmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
